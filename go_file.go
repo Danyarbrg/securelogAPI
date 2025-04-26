30051bef4b29914b24b89ab731e5cc40847361f5
Rayberg <neformat30@gmail.com>
2025-04-26 14:44:31 +0300

    added go file

---
+++ go_file.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func work() {
+	fmt.Println("Time to codding!")
+	var num = math.Sqrt(10)
+	fmt.Printf("Sqrt of 10 is %v\n", num)
+}
+
+func main() {
+	work()
+}
